0ccf6750697e3e5dc05912c6b9d7044b7b04f831
PrasiddhaKhadka <prasiddhak77@gmail.com>
2026-01-11 12:38:33 +0545

    Add: setting up the const

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Nstore';
+export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'A modern ecommerce site built with Next.js';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
